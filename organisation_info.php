c27eb26ae7c43bb38cbef80e647cf1ed2a92fa79
Peng Shensong <pss9204@hotmail.com>
2014-08-13 17:14:20 +0800

    Organisation Info page and added a class to css

--- organisation_info.php
+++ organisation_info.php
@@ -10,7 +10,7 @@
             <div class="col-md-4">
                 <!-- If you do not want a link in the header, instead of .header-title-link you can use a div with the class .header-section -->
                 <a href="" class="header-title-link">
-                    <h1><i class="fa fa-coffee animation-expandUp"></i>Hall 11<br><small>Welcome admin!</small></h1>
+                    <h1>Hall 11<br><small>Welcome admin!</small></h1>
                 </a>
             </div>
             <!-- END Title -->
@@ -45,7 +45,7 @@
                             <div class="col-xs-6">
                                 <a href="page_special_timeline.php" class="header-link">
                                     <h1 class="animation-pullDown">
-                                        <strong>0</strong><br><small>Events</small>
+                                        <strong>1</strong><br><small>Events</small>
                                     </h1>
                                 </a>
                             </div>
@@ -64,63 +64,44 @@
             </div>
             <!-- END Statistics -->
         </div>
-    </div>
-    <ul class="breadcrumb breadcrumb-top">
-        <li><i class="fa fa-coffee"></i></li>
-        <li><a href="">Dashboard</a></li>
-    </ul>
+		<!-- Action buttons -->
+			<div>
+			<a href='organisation_info.php'><button type="button">Create Event</button>
+			<a href='organisation_info.php'><button type="button">Add Member</button>
+			<a href='organisation_info.php'><button type="button">View Members</button>
+			</div>
+		<!-- End of Action buttons -->
+    </div>	
     <!-- END Dashboard Header -->
-
     <!-- Dashboard Content -->
     <div class="row gutter30">
-        <div class="col-md-6">
-            <!-- Search Block -->
-            <div class="block full">
-                <div class="block-title">
-                    <div class="block-options pull-right">
-                        <div class="btn-group btn-group-sm">
-                            <a href="javascript:void(0)" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Everything <span class="caret"></span></a>
-                            <ul class="dropdown-menu dropdown-menu-right">
-                                <li class="active"><a href="javascript:void(0)"><i class="fa fa-check"></i> Everything</a></li>
-                                <li class="divider"></li>
-                                <li><a href="javascript:void(0)">Users</a></li>
-                                <li><a href="javascript:void(0)">Projects</a></li>
-                                <li><a href="javascript:void(0)">Sites</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <h2><i class="fa fa-search"></i> Search</h2>
-                </div>
-                <form action="page_ready_search_results.php" method="post">
-                    <div class="input-group">
-                        <input type="text" id="search-term" name="search-term" class="form-control" placeholder="What?">
-                        <span class="input-group-btn">
-                            <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-                        </span>
-                    </div>
-                </form>
-            </div>
-            <!-- END Search Block -->
+		<div class="col-md-6">
 
             <!-- Recent Updates Block -->
             <div class="block full">
                 <!-- Recent Updates Title -->
                 <div class="block-title">
-                    <h2><i class="fa fa-rss"></i> News</h2>
+                    <h2><i class="fa fa-rss"></i> Profile</h2>
                 </div>
                 <!-- END Recent Updates Title -->
 
-                <!-- Update Status Form -->
-                <form action="index.php" method="post" class="profile-status block-top" onsubmit="return false;">
-                    <textarea id="status-update" name="status-update" rows="3" class="form-control" placeholder="How are you? (Try posting something)"></textarea>
+                <!-- Update Profile Form -->
+				<div>
+				Hi there, we are a group of students from Hall 11 of NTU. We are actively looking for sponsors to help sponsor
+				some of our upcoming event, thanks for visiting our profile.
+				.
+				.
+				.
+				</div>
+                <br><form action="index.php" method="post" class="profile-status block-top" onsubmit="return false;">
                     <div class="clearfix">
-                        <button type="submit" id="status-update-btn" class="btn btn-primary pull-right"><i class="fa fa-angle-right"></i> Post</button>
+                        <button type="submit" id="status-update-btn" class="btn btn-primary pull-right"><i class="fa fa-angle-right"></i> Edit</button>
                         <a href="javascript:void(0)" class="btn btn-link btn-icon" data-toggle="tooltip" data-placement="bottom" title="Add Location"><i class="fa fa-location-arrow"></i></a>
                         <a href="javascript:void(0)" class="btn btn-link btn-icon" data-toggle="tooltip" data-placement="bottom" title="Record Voice"><i class="fa fa-microphone"></i></a>
                         <a href="javascript:void(0)" class="btn btn-link btn-icon" data-toggle="tooltip" data-placement="bottom" title="Take Photo"><i class="fa fa-camera"></i></a>
                         <a href="javascript:void(0)" class="btn btn-link btn-icon" data-toggle="tooltip" data-placement="bottom" title="Upload File"><i class="fa fa-cloud-upload"></i></a>
                     </div>
-                </form>
+                </br></form>
                 <!-- END Update Status Form -->
 
                 <!-- Updates -->
@@ -128,57 +109,13 @@
                     <ul class="timeline remove-margin">
                         <li class="alt-color">
                             <div class="timeline-item">
-                                <h4 class="timeline-title"><small class="timeline-meta">3 min ago</small><i class="fa fa-plus"></i> Friend Request</h4>
+                                <h4 class="timeline-title"><small class="timeline-meta">3 min ago</small><i class="fa fa-plus"></i> Added Event</h4>
                                 <div class="timeline-content">
                                     <p class="clearfix">
                                         <img src="img/template/avatar.png" alt="avatar" class="img-circle pull-right">
-                                        <a href="page_special_user_profile.php">John</a> would like to become friends!
+                                        <a href="page_special_user_profile.php">Sports</a> Soccer game!
                                     </p>
-                                    <a href="javascript:void(0)" class="btn btn-xs btn-success" id="accept-request"><i class="fa fa-check"></i> Accept</a>
                                 </div>
-                            </div>
-                        </li>
-                        <li class="text-right">
-                            <div class="timeline-item">
-                                <h4 class="timeline-title"><small class="timeline-meta">20 min ago</small><i class="fa fa-file"></i> Status</h4>
-                                <div class="timeline-content">Today is a good day!</div>
-                            </div>
-                        </li>
-                        <li class="text-right">
-                            <div class="timeline-item">
-                                <h4 class="timeline-title"><small class="timeline-meta">1 hour ago</small><i class="fa fa-cloud-upload"></i> Image Upload</h4>
-                                <div class="timeline-content">
-                                    <p>
-                                        <a href="page_special_user_profile.php">Michael</a> just uploaded an image in <a href="javascript:void(0)" class="btn btn-xs btn-primary">Secret Project</a>
-                                    </p>
-                                    <a href="img/placeholders/image_720x450_dark.png" data-toggle="lightbox-image">
-                                        <img src="img/placeholders/image_160x120_dark.png" alt="image">
-                                    </a>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="alt-color">
-                            <div class="timeline-item">
-                                <h4 class="timeline-title"><small class="timeline-meta">2 hours ago</small><i class="fa fa-cog"></i> System Update</h4>
-                                <div class="timeline-content"><strong>Application</strong> updated to version 3.0! Check out the <a href="javascript:void(0)">documentation</a> to learn about the new features!</div>
-                            </div>
-                        </li>
-                        <li class="text-right">
-                            <div class="timeline-item">
-                                <h4 class="timeline-title"><small class="timeline-meta">3 hours ago</small><i class="fa fa-magic"></i> Special Offer</h4>
-                                <div class="timeline-content"><a href="page_ready_pricing_tables.php">Upgrade your plan</a> and get 1 year for free!</div>
-                            </div>
-                        </li>
-                        <li class="text-right">
-                            <div class="timeline-item">
-                                <h4 class="timeline-title"><small class="timeline-meta">1 day ago</small><i class="fa fa-file"></i> Status</h4>
-                                <div class="timeline-content">Just finished the project, check it out <a href="javascript:void(0)">live</a>!</div>
-                            </div>
-                        </li>
-                        <li class="alt-color">
-                            <div class="timeline-item">
-                                <h4 class="timeline-title"><small class="timeline-meta">2 days ago</small><i class="fa fa-hdd-o"></i> Server Update</h4>
-                                <div class="timeline-content"><strong>Server #1</strong> updated successfully!</div>
                             </div>
                         </li>
                     </ul>
@@ -188,54 +125,18 @@
             <!-- END Recent Updates Block -->
         </div>
         <div class="col-md-6">
-            <!-- Quick Stats Block -->
-            <div class="block">
-                <!-- Quick Stats Title -->
-                <div class="block-title">
-                    <h2 class="text-center"><i class="fa fa-suitcase"></i> Business Plan <small>Active - <a href="page_ready_pricing_tables.php">Upgrade?</a></small></h2>
-                </div>
-                <!-- END Quick Stats Title -->
-
-                <!-- Quick Stats Content -->
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="pie-chart block-section" data-percent="90" data-size="150">
-                            <span>4.5<small>/5</small> <strong>GB</strong></span>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="pie-chart block-section" data-percent="60" data-size="150">
-                            <span>12<small>/20</small> <strong>Sites</strong></span>
-                        </div>
-                    </div>
-                </div>
-                <!-- END Quick Stats Content -->
-            </div>
-            <!-- END Quick Stats Block -->
-
-            <!-- Earning/Sales Stats Block -->
-            <div class="block full">
-                <div class="block-title">
-                    <h2 class="text-center"><i class="gi gi-charts"></i> Earnings / Sales <small>Last Week - <a href="page_comp_charts.php">In detail</a></small></h2>
-                </div>
-                <div id="chart-classic" class="chart"></div>
-            </div>
-            <!-- END Earning/Sales Stats Block -->
 
             <!-- Extra Block -->
             <div class="block">
                 <!-- Extra Title -->
                 <div class="block-title">
-                    <h2><i class="fa fa-thumbs-o-up"></i> Recommended Web Designers</h2>
+                    <h2></i>Organisation Logo</h2>
                 </div>
                 <!-- END Extra Title -->
 
                 <!-- Extra Content -->
                 <div class="content-text clearfix">
-                    <img src="img/template/avatar2.jpg" alt="avatar" class="img-circle img-responsive pull-left">
-                    <h3 class="push"><a href="page_special_user_profile.php">John Doe</a> <small><em>750 Followers</em></small></h3>
-                    <a href="javascript:void(0)" class="btn btn-xs btn-success"><i class="fa fa-share"></i> Follow</a>
-                    <a href="javascript:void(0)" class="btn btn-xs btn-primary">Hire</a>
+                    <img src="img/template/hall 11.jpg" alt="avatar" class="img-circle img-responsive pull-left">
                 </div>
                 <!-- END Extra Content -->
             </div>
