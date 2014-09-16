9d12a24d235609ed5db640fb904374c824df2dfb
jervenne <j0y5@hotmail.com>
2014-09-16 23:54:26 +0800

    updated event listing UI and DB

--- NEW-UI/event-listings.php
+++ NEW-UI/event-listings.php
@@ -12,7 +12,7 @@ include 'inc/dbconfig.php';
         <!-- Add the category value you want each link in .gallery-filter to filter out in its data-category attribute. Add the value 'all' to show all items -->
         <div class="header-section text-center">
             <div class="btn-group gallery-filter">
-                <a href="javascript:void(0)" class="btn btn-effect-ripple btn-primary" data-category="preferreds">My Prefered</a>
+                <a href="javascript:void(0)" class="btn btn-effect-ripple btn-primary" data-category="preferred">My Preferred</a>
                 <a href="javascript:void(0)" class="btn btn-effect-ripple btn-primary active" data-category="all">All</a>
                 <a href="javascript:void(0)" class="btn btn-effect-ripple btn-primary" data-category="new">New</a>
                 <a href="javascript:void(0)" class="btn btn-effect-ripple btn-primary" data-category="popular">Popular</a>
@@ -20,74 +20,107 @@ include 'inc/dbconfig.php';
             </div>
         </div>
         <!-- END Gallery Filter Links -->
+        <!--
+        <div class="alert alert-info alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4>Do you know that you filter to your preferred event type? To customize your filters, click here.</h4>
+        </div>
+    -->
     </div>
     <!-- END Gallery Header -->
 
     <!-- Gallery Items -->
-    <!-- Lightbox functionality is based on Magnific Popup plugin (initialized in js/app.js -> uiInit()) -->
-    <!-- Add the category value for each item in its data-category attribute (for the filter functionality to work) -->
     <div class="row gallery">
-
         <div class="col-sm-4">
-        <div class="gallery-image-container animation-fadeInQuick2" data-category="new">
-            <a href="page_ready_article.html" class="widget">
-                <div class="widget-content themed-background text-light-op grid-header">
-                    <span class="pull-right"><i class="fa fa-users"></i>20000</span>
-                    <i class="fa fa-fw fa-file-text"></i> <strong>Concert</strong>
-                </div>
-                <div class="widget-image widget-image-sm">
-                    <img src="img/placeholders/photos/photo19.jpg" alt="image">
-                    <div class="widget-image-content">
+            <div class="gallery-image-container animation-fadeInQuick2" data-category="new">
+                <a href="page_ready_article.html" class="widget">
+                    <div class="widget-content themed-background text-light-op grid-header">
+                        <span class="pull-right"><i class="fa fa-users"></i>20000</span>
+                        <i class="fa fa-fw fa-file-text"></i> <strong>Concert</strong>
                     </div>
-                </div>
-                <div class="widget-content text-dark">
-                    <div class="g-row">
-                        <span class="pull-right">14 Oct 2014</span>
-                        <h3 class="widget-heading"><strong>Big Shot Workshop</strong></h3>
+                    <div class="widget-image widget-image-sm">
+                        <img src="img/placeholders/photos/photo19.jpg" alt="image">
+                        <div class="widget-image-content">
+                        </div>
                     </div>
-                    <div class="g-row">
-                        <h5 class="widget-heading"><em>by Standard Chartered</em></h5>
+                    <div class="widget-content text-dark">
+                        <div class="g-row">
+                            <span class="pull-right">14 Oct 2014</span>
+                            <h3 class="widget-heading"><strong>Big Shot Workshop</strong></h3>
+                        </div>
+                        <div class="g-row">
+                            <h5 class="widget-heading"><em>by Standard Chartered</em></h5>
+                        </div>
+                        <div class="g-row event-desc">
+                            <h4 class="widget-heading">Wanna be a bigshot? You have to attend this. Fun games and informative session!</h4>
+                        </div>
+                        <div class="g-row">
+                            <h6 class="widget-heading"><i class="fa fa-tags"></i> <span class="label label-default">Young Professionals</span>, <span class="label label-default">General Crowd</span>, <span class="label label-default">Seniors</span></h6>
+                        </div>
                     </div>
-                    <div class="g-row">
-                        <h4 class="widget-heading">Wanna be a bigshot? You have to attend this. Fun games and informative session!</h4>
-                    </div>
-                    <div class="g-row">
-                        <h6 class="widget-heading"><i class="fa fa-tags"></i> <span class="label label-default">Young Professionals</span>, <span class="label label-default">General Crowd</span>, <span class="label label-default">Seniors</span></h6>
-                    </div>
-                </div>
-            </a>
+                </a>
+            </div>
         </div>
-    </div>
         <div class="col-sm-4">
-        <div class="gallery-image-container animation-fadeInQuick2" data-category="popular">
-            <a href="page_ready_article.html" class="widget">
-                <div class="widget-content themed-background text-light-op grid-header">
-                    <span class="pull-right"><i class="fa fa-users"></i>20000</span>
-                    <i class="fa fa-fw fa-file-text"></i> <strong>Concert</strong>
-                </div>
-                <div class="widget-image widget-image-sm">
-                    <img src="img/placeholders/photos/photo19.jpg" alt="image">
-                    <div class="widget-image-content">
+            <div class="gallery-image-container animation-fadeInQuick2" data-category="popular">
+                <a href="page_ready_article.html" class="widget">
+                    <div class="widget-content themed-background text-light-op grid-header">
+                        <span class="pull-right"><i class="fa fa-users"></i>20000</span>
+                        <i class="fa fa-fw fa-file-text"></i> <strong>Concert</strong>
                     </div>
-                </div>
-                <div class="widget-content text-dark">
-                    <div class="g-row">
-                        <span class="pull-right">14 Oct 2014</span>
-                        <h3 class="widget-heading"><strong>Big Shot Workshop</strong></h3>
+                    <div class="widget-image widget-image-sm">
+                        <img src="img/placeholders/photos/photo19.jpg" alt="image">
+                        <div class="widget-image-content">
+                        </div>
                     </div>
-                    <div class="g-row">
-                        <h5 class="widget-heading"><em>by Standard Chartered</em></h5>
+                    <div class="widget-content text-dark">
+                        <div class="g-row">
+                            <span class="pull-right">14 Oct 2014</span>
+                            <h3 class="widget-heading"><strong>Big Shot Workshop</strong></h3>
+                        </div>
+                        <div class="g-row">
+                            <h5 class="widget-heading"><em>by Standard Chartered</em></h5>
+                        </div>
+                        <div class="g-row event-desc">
+                            <h4 class="widget-heading">Wanna be a bigshot? You have to attend this. Fun games and informative session!</h4>
+                        </div>
+                        <div class="g-row">
+                            <h6 class="widget-heading"><i class="fa fa-tags"></i> <span class="label label-default">Young Professionals</span>, <span class="label label-default">General Crowd</span>, <span class="label label-default">Seniors</span></h6>
+                        </div>
                     </div>
-                    <div class="g-row">
-                        <h4 class="widget-heading">Wanna be a bigshot? You have to attend this. Fun games and informative session!</h4>
-                    </div>
-                    <div class="g-row">
-                        <h6 class="widget-heading"><i class="fa fa-tags"></i> <span class="label label-default">Young Professionals</span>, <span class="label label-default">General Crowd</span>, <span class="label label-default">Seniors</span></h6>
-                    </div>
-                </div>
-            </a>
+                </a>
+            </div>
         </div>
-    </div>
+        <div class="col-sm-4">
+            <div class="gallery-image-container animation-fadeInQuick2" data-category="soon">
+                <a href="page_ready_article.html" class="widget">
+                    <div class="widget-content themed-background text-light-op grid-header">
+                        <span class="pull-right"><i class="fa fa-users"></i>20000</span>
+                        <i class="fa fa-fw fa-file-text"></i> <strong>Concert</strong>
+                    </div>
+                    <div class="widget-image widget-image-sm">
+                        <img src="img/placeholders/photos/photo19.jpg" alt="image">
+                        <div class="widget-image-content">
+                        </div>
+                    </div>
+                    <div class="widget-content text-dark">
+                        <div class="g-row">
+                            <span class="pull-right">14 Oct 2014</span>
+                            <h3 class="widget-heading"><strong>Big Shot Workshop</strong></h3>
+                        </div>
+                        <div class="g-row">
+                            <h5 class="widget-heading"><em>by Standard Chartered</em></h5>
+                        </div>
+                        <div class="g-row event-desc">
+                            <h4 class="widget-heading">Wanna be a bigshot? You have to attend this. Fun games and informative session!</h4>
+                        </div>
+                        <div class="g-row">
+                            <h6 class="widget-heading"><i class="fa fa-tags"></i> <span class="label label-default">Young Professionals</span>, <span class="label label-default">General Crowd</span>, <span class="label label-default">Seniors</span></h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
         <?php
 
         $sql = "SELECT * FROM event";
@@ -96,60 +129,63 @@ include 'inc/dbconfig.php';
         while ($row = mysql_fetch_array($result)) { 
 
         //get event id
-        $event_id = $row['id'];
+            $event_id = $row['id'];
 
+            ?>
+            <div class="col-sm-4">
+                <div class="gallery-image-container animation-fadeInQuick2" data-category="soon">
+                    <a href="page_ready_article.html" class="widget">
+                        <div class="widget-content themed-background text-light-op grid-header">
+                            <span class="pull-right"><i class="fa fa-users"></i><?=$row['turnout']?></span>
+                            <i class="fa fa-fw fa-file-text"></i> <strong>
+            <?
             //get list of event types
             $sql1 = "SELECT type FROM events_type et, type_of_events toe WHERE et.event_type_id=toe.id AND event_id=".$event_id;
             $result1 = mysql_query($sql1);
 
             while ($row1 = mysql_fetch_array($result1)) {
 
+                if(mysql_next_result()){ echo','; }
             }
 
         //display the event info
 
             //get list of target audience
             $sql2 = "SELECT type FROM events_audience ea, target_audience ta WHERE ea.audience_id=ta.id AND event_id=".$event_id;
-                $result2 = mysql_query($sql2);
+            $result2 = mysql_query($sql2);
 
-                while ($row2 = mysql_fetch_array($result2)) {
+            while ($row2 = mysql_fetch_array($result2)) {
 
             }
         }
-    ?>
-    <div class="col-sm-4">
-        <div class="gallery-image-container animation-fadeInQuick2" data-category="soon">
-            <a href="page_ready_article.html" class="widget">
-                <div class="widget-content themed-background text-light-op grid-header">
-                    <span class="pull-right"><i class="fa fa-users"></i>20000</span>
-                    <i class="fa fa-fw fa-file-text"></i> <strong>Concert</strong>
-                </div>
-                <div class="widget-image widget-image-sm">
-                    <img src="img/placeholders/photos/photo19.jpg" alt="image">
-                    <div class="widget-image-content">
+        ?>
+        Concert</strong>
                     </div>
-                </div>
-                <div class="widget-content text-dark">
-                    <div class="g-row">
-                        <span class="pull-right">14 Oct 2014</span>
-                        <h3 class="widget-heading"><strong>Big Shot Workshop</strong></h3>
+                    <div class="widget-image widget-image-sm">
+                        <img src="img/placeholders/photos/photo19.jpg" alt="image">
+                        <div class="widget-image-content">
+                        </div>
                     </div>
-                    <div class="g-row">
-                        <h5 class="widget-heading"><em>by Standard Chartered</em></h5>
+                    <div class="widget-content text-dark">
+                        <div class="g-row">
+                            <span class="pull-right">14 Oct 2014</span>
+                            <h3 class="widget-heading"><strong>Big Shot Workshop</strong></h3>
+                        </div>
+                        <div class="g-row">
+                            <h5 class="widget-heading"><em>by Standard Chartered</em></h5>
+                        </div>
+                        <div class="g-row event-desc">
+                            <h4 class="widget-heading">Wanna be a bigshot? You have to attend this. Fun games and informative session!</h4>
+                        </div>
+                        <div class="g-row">
+                            <h6 class="widget-heading"><i class="fa fa-tags"></i> <span class="label label-default">Young Professionals</span>, <span class="label label-default">General Crowd</span>, <span class="label label-default">Seniors</span></h6>
+                        </div>
                     </div>
-                    <div class="g-row">
-                        <h4 class="widget-heading">Wanna be a bigshot? You have to attend this. Fun games and informative session!</h4>
-                    </div>
-                    <div class="g-row">
-                        <h6 class="widget-heading"><i class="fa fa-tags"></i> <span class="label label-default">Young Professionals</span>, <span class="label label-default">General Crowd</span>, <span class="label label-default">Seniors</span></h6>
-                    </div>
-                </div>
-            </a>
+                </a>
+            </div>
         </div>
     </div>
-
-</div>
-<!-- END Gallery Items -->
+    <!-- END Gallery Items -->
 </div>
 <!-- END Page Content -->
 </div>
--- sponsorsDB.sql
+++ sponsorsDB.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 09, 2014 at 08:40 AM
+-- Generation Time: Sep 16, 2014 at 03:52 PM
 -- Server version: 5.1.44
 -- PHP Version: 5.3.1
 
@@ -28,27 +28,31 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE TABLE IF NOT EXISTS `event` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `event_name` varchar(45) NOT NULL,
-  `logo` varchar(45) DEFAULT NULL,
-  `address` varchar(45) NOT NULL,
+  `logo` varchar(200) DEFAULT NULL,
+  `location` varchar(45) NOT NULL,
   `turnout` varchar(45) NOT NULL,
   `description` varchar(999) NOT NULL,
   `org_info` varchar(500) DEFAULT NULL,
   `start_date` varchar(45) DEFAULT NULL,
   `end_date` varchar(45) DEFAULT NULL,
-  `start_time` varchar(25) NOT NULL,
-  `end_time` varchar(25) NOT NULL,
+  `start_time` varchar(45) NOT NULL,
+  `end_time` varchar(45) NOT NULL,
   `facebook` varchar(45) DEFAULT NULL,
   `facebook_event` varchar(45) DEFAULT NULL,
   `twitter` varchar(45) DEFAULT NULL,
   `instagram` varchar(45) DEFAULT NULL,
   `website` varchar(45) DEFAULT NULL,
+  `created_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `event`
 --
 
+INSERT INTO `event` (`id`, `event_name`, `logo`, `location`, `turnout`, `description`, `org_info`, `start_date`, `end_date`, `start_time`, `end_time`, `facebook`, `facebook_event`, `twitter`, `instagram`, `website`, `created_date`) VALUES
+(1, 'sdfsdfs', 'img/logos/hearts.jpg', 'home', '233', 'wrqweqw', 'qweqwe', '08/30/2014', '09/18/2014', '1:50:15 AM', '1:50:15 AM', '', '', '', '', '', '2014-09-15 02:16:29'),
+(2, 'sdfsdfs', 'img/logos/hearts.jpg', 'home', '233', 'wrqweqw', 'qweqwe', '08/30/2014', '09/18/2014', '1:50:15 AM', '1:50:15 AM', '', '', '', '', '', '2014-09-15 02:16:37');
 
 -- --------------------------------------------------------
 
@@ -57,17 +61,21 @@ CREATE TABLE IF NOT EXISTS `event` (
 --
 
 CREATE TABLE IF NOT EXISTS `events_audience` (
-  `events_id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
   `audience_id` int(11) NOT NULL,
-  PRIMARY KEY (`events_id`,`audience_id`),
+  PRIMARY KEY (`event_id`,`audience_id`),
   KEY `fk_events_has_target_audience_target_audience1` (`audience_id`),
-  KEY `fk_events_has_target_audience_events` (`events_id`)
+  KEY `fk_events_has_target_audience_events` (`event_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `events_audience`
 --
 
+INSERT INTO `events_audience` (`event_id`, `audience_id`) VALUES
+(2, 1),
+(2, 9),
+(2, 13);
 
 -- --------------------------------------------------------
 
@@ -87,6 +95,15 @@ CREATE TABLE IF NOT EXISTS `events_type` (
 -- Dumping data for table `events_type`
 --
 
+INSERT INTO `events_type` (`event_id`, `event_type_id`) VALUES
+(1, 15),
+(2, 15),
+(1, 16),
+(2, 16),
+(1, 19),
+(2, 19),
+(1, 20),
+(2, 20);
 
 -- --------------------------------------------------------
 
@@ -120,12 +137,26 @@ CREATE TABLE IF NOT EXISTS `target_audience` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `target_audience`
 --
 
+INSERT INTO `target_audience` (`id`, `type`) VALUES
+(1, 'Teenagers'),
+(2, 'Children'),
+(3, 'Polytechnic Students'),
+(4, 'University Students'),
+(5, 'Young Professionals'),
+(6, 'Seniors'),
+(7, 'Sports'),
+(8, 'Music'),
+(9, 'Theatre'),
+(10, 'Business people'),
+(11, 'Foodies'),
+(12, 'Fashionistas'),
+(13, 'Shows');
 
 -- --------------------------------------------------------
 
@@ -153,7 +184,6 @@ INSERT INTO `type_of_events` (`id`, `type`) VALUES
 (7, 'food'),
 (8, 'gala'),
 (9, 'social'),
-(10, ''),
 (11, 'movie'),
 (12, 'networking'),
 (13, 'party'),
@@ -183,7 +213,7 @@ INSERT INTO `type_of_events` (`id`, `type`) VALUES
 -- Constraints for table `events_audience`
 --
 ALTER TABLE `events_audience`
-  ADD CONSTRAINT `fk_events_has_target_audience_events` FOREIGN KEY (`events_id`) REFERENCES `event` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_events_has_target_audience_events` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_events_has_target_audience_target_audience1` FOREIGN KEY (`audience_id`) REFERENCES `target_audience` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
